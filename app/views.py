8ef3f0b9c2420203417d7ff8fb81d7e6c06b73b9
Davaskye DeBique <Davaskye@gmail.com>
2022-02-14 22:42:54 -0400

    Working version

--- app/views.py
+++ app/views.py
@@ -7,6 +7,9 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -23,6 +26,23 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page"""
+    form = ContactForm()
+    if request.method=="POST" and form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        msg = Message(subject, sender=(name,email),recipients=["davaskye@awesome.com"])
+        msg.body = message
+        mail.send(msg)
+        flash('You successfully filled out the form!')
+        return redirect(url_for('home'))
+    return render_template('contact.html', form=form)
 
 
 ###
